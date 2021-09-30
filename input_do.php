525ea8faf16e6869b787e06ded2e687f0295f719
sakasegawajunki <80205010+sakasegawajunki@users.noreply.github.com>
2021-09-30 22:01:28 +0900

    フォームからの情報を保存すする②

--- input_do.php
+++ input_do.php
@@ -21,8 +21,9 @@
   <?php
   try {
     $db = new PDO('mysql:dbname=mydb;host=localhost; port=8889; charset=utf8','root','root');
-
-    $db->exec('INSERT INTO memos SET memo="' . $_POST['memo'] . '", created_at=NOW()');
+    $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+    $statement->execute(array($_POST['memo']));
+    echo "メッセージが登録されました";
   } catch(PDOException $e) {
     echo "DB接続エラー: " . $e->getMessage();
   }
