3ce78e4ba032c93cce6becb7c9b983aafe3c9a3a
sakasegawajunki <80205010+sakasegawajunki@users.noreply.github.com>
2021-10-01 15:34:30 +0900

    共通プログラム作るを

--- index.php
+++ index.php
@@ -20,12 +20,7 @@
 
   
 <?php
- try {
-  $db = new PDO('mysql:dbname=mydb;host=localhost; port=8889; charset=utf8','root','root');
-}catch(PDOException $e){
-echo 'DB接続エラー:'.$e->getMessage();
-}
-
+require("dbconnect.php");
 $memos = $db->query('SELECT * FROM memos ORDER BY id DESC');
 ?>
 
--- memo.php
+++ memo.php
@@ -20,11 +20,7 @@
 
   
 <?php
- try {
-  $db = new PDO('mysql:dbname=mydb;host=localhost; port=8889; charset=utf8','root','root');
-}catch(PDOException $e){
-echo 'DB接続エラー:'.$e->getMessage();
-}
+require("dbconnect.php");
 
 $id = $_REQUEST["id"];
 if (!is_numeric($id) || $id <= 0) {
