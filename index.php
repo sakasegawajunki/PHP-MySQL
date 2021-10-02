8262c7df7c08979b21898fc4497380de5c82faf3
sakasegawajunki <80205010+sakasegawajunki@users.noreply.github.com>
2021-10-02 20:45:11 +0900

    件数の多いレコードを、ページを分ける「ページング（ページネーション）」②

--- index.php
+++ index.php
@@ -17,35 +17,34 @@
 
 <main>
 <h2>Practice</h2>
-
-  
 <?php
-require("dbconnect.php");
+require('dbconnect.php');
 
-if (isset($_REQUEST["page"]) && is_numeric($_REQUEST["page"])) {
-    $page = $_REQUEST["page"];
-} else
-    $page = 1;
+if (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])) {
+  $page = $_REQUEST['page'];
+} else {
+  $page = 1;
 }
-
-
-$start = 5* ($page - 1);
+$start = 5 * ($page - 1);
 
 $memos = $db->prepare('SELECT * FROM mydb.memos ORDER BY id DESC LIMIT ?, 5');
-$memos->bindParam(1,$start, PDO::PARAM_INT);
+$memos->bindParam(1, $start, PDO::PARAM_INT);
 $memos->execute();
-
-
 ?>
-
+ 
 <article>
-  <?php while ($memo = $memos->fetch()): ?>
-    <p><a href="memo.php?id=<?php print($memo['id']); ?>"><?php print(mb_substr($memo['memo'],0,50)); ?></a></p>
-    <time><?php print($memo['created_at']); ?></time>
-    <hr>
-  <?php endwhile; ?>
-</article>
+    <?php while ($memo = $memos->fetch()): ?>
+        <p><a href="memo.php?id=<?php print($memo['id']); ?>"><?php print(mb_substr($memo['memo'], 0, 50)); ?></a></p>
+        <time><?php print($memo['created_at']); ?></time>
+        <hr>
+        <?php endwhile; ?>
 
+      <?php if ($page >= 2): ?>
+        <a href= "index.php?page=<?php print($page-1); ?>"><?php print($page-1); ?>ページ目へ</a>
+      <?php endif; ?>
+      |
+      <a href= "index.php?page=<?php print($page+1); ?>"><?php print($page+1); ?>ページ目へ</a>
+    </article>       
 </main>
 </body>    
 </html>
